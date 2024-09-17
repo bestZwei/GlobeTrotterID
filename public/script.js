dfc78b1b860ea9c7ba26d5a52a7c3bab7ba1b0ec
developer <noreply@null.org>
2024-09-17 09:14:40 +0800

    更新

--- public/script.js
+++ public/script.js
@@ -58,6 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
       "CA": "+1",
       "CH": "+41",
       "DE": "+49",
+      "DK": "+45",  
       "ES": "+34",
       "FI": "+358",
       "FR": "+33",
@@ -67,7 +68,9 @@ document.addEventListener('DOMContentLoaded', () => {
       "IR": "+98",
       "MX": "+52",
       "NL": "+31",
+      "NO": "+47",  
       "NZ": "+64",
+      "RS": "+381",  
       "TR": "+90",
       "UA": "+380",
       "US": "+1"
